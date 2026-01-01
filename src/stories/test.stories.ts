8ffcac92ff0aa7478bdc4c01f5734453e625507b
Stukova Olya <stukova.o@gmail.com>
2026-01-01 09:10:57 +0500

    Remove test-luma-migration example
    Signed-off-by: Stukova Olya <stukova.o@gmail.com>

--- src/stories/test.stories.ts
+++ src/stories/test.stories.ts
@@ -1,24 +1,10 @@
 import type { Meta } from '@storybook/html'
 
-import { createStory, Story } from '@/graph/stories/create-story'
 import { CosmosStoryProps } from './create-cosmos'
-import { testLumaMigration } from './test-luma-migration'
-
-import testLumaMigrationRaw from './test-luma-migration?raw'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<CosmosStoryProps> = {
   title: 'Tests/Luma Migration',
-}
-
-export const TestLumaMigration: Story = {
-  ...createStory(testLumaMigration),
-  name: 'Test Luma.gl Migration',
-  parameters: {
-    sourceCode: [
-      { name: 'Story', code: testLumaMigrationRaw },
-    ],
-  },
 }
 
 // eslint-disable-next-line import/no-default-export
